ea0784ac6f8393993f366e6ceb96e739a4bc72ef
Chatree.js <36321701+chatreejs@users.noreply.github.com>
2024-08-27 21:36:35 +0700

    chore: fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,60 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import { fixupPluginRules } from '@eslint/compat';
+import eslint from '@eslint/js';
+import configPrettier from 'eslint-config-prettier';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import plunginReactRefresh from 'eslint-plugin-react-refresh';
+import pluginReactConfigRecommended from 'eslint-plugin-react/configs/recommended.js';
+import globals from 'globals';
+import { dirname } from 'path';
+import tseslint from 'typescript-eslint';
+import { fileURLToPath } from 'url';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    ...pluginReactConfigRecommended,
+    ...pluginReactHooks.configs.recommended,
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react: fixupPluginRules(pluginReactConfigRecommended.plugins.react),
+      'react-hooks': fixupPluginRules(pluginReactHooks),
+      'react-refresh': plunginReactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ...pluginReactConfigRecommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      ...pluginReactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': 'error',
+    },
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: dirname(fileURLToPath(import.meta.url)),
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
     },
   },
-)
+  {
+    ignores: [
+      'dist/*',
+      'vite.config.ts',
+      'eslint.config.js',
+      'babel.config.js',
+      'jest.config.ts',
+    ],
+  },
+  configPrettier, // For disabling conflicting rules with Prettier, require as the last config
+);
